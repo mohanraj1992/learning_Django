ef51ae5a175c03002b1afaf1b66f9220752f46f1
mohanraj1992 <kctcsmohan@gmail.com>
2017-09-14 17:24:53 +0530

    adding changes for tutorial 22, 23, 24 ==> creation of forms

--- music/views.py
+++ music/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
 
-from .models import Album
+from .models import Album, Song
 from django.shortcuts import render
 from django.http import Http404
 
@@ -34,3 +34,14 @@ def detail(request, album_id):
     
     album = get_object_or_404(Album , pk=album_id)
     return render(request,'music/detail.html', {'album': album})
+
+def favourite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {'album': album, 'error_message':"You did not select a valid song"})
+    else:
+        selected_song.is_favourite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
