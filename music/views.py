392e1755ff4474423d41f449968d70a879a89b8c
mohanraj1992 <kctcsmohan@gmail.com>
2017-09-15 17:54:01 +0530

    adding code for generic views tutorial - 30

--- music/views.py
+++ music/views.py
@@ -1,47 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
-from django.http import HttpResponse
-
-from .models import Album, Song
-from django.shortcuts import render
-from django.http import Http404
+from django.views import generic
+from .models import Album
 
 
-def index(request):
-    all_albums = Album.objects.all()
-    #commented out are old methods of creating
-    #html = ''
-    #for album in all_albums:
-    #    url = '/music/' + str(album.id) + '/'
-    #    html += '<a href="'+url+'">'+album.album_title + '</a><br>'
-    #return HttpResponse(html)
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
 
-    #Below changes are made for tutorial 15
-    context = {'all_albums' : all_albums}
-    return render(request, 'music/index.html', context)
+    def get_queryset(self):
+        return Album.objects.all()
 
-
-
-
-def detail(request, album_id):
-
-    #this is for old
-    #return HttpResponse("<h2>Details for Album id:" + str(album_id) + "</h2>")
-    
-    album = get_object_or_404(Album , pk=album_id)
-    return render(request,'music/detail.html', {'album': album})
-
-def favourite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {'album': album, 'error_message':"You did not select a valid song"})
-    else:
-        selected_song.is_favourite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
